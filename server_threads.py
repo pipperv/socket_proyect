41076f973f8b6b64fc1518cdc641e49ddacee15c
Felipe Valenzuela R <felipevalenzuela@ug.uchile.cl>
2021-10-11 21:47:12 -0300

    part 5 done

--- server_threads.py
+++ server_threads.py
@@ -13,8 +13,33 @@ with open("artefactos.json", "r") as j:
 def cliente(sock):
     global sock_clientes, arte_dict, artefactos
 
+    #Ask for artefacts
+    arte = None
+    while arte == None:
+        sock.send("[SERVER] Cuéntame, ¿qué artefactos tienes?".encode())
+        try:
+            answ = sock.recv(1024).decode()
+        except:
+            break
+        answ_list = answ.split(',')
+        art_list = [artefactos[k] for k in answ_list]
+        if len(art_list) > 6:
+            sock.send("[SERVER] No puedes tener mas de 6 artefactos!".encode())
+        else:
+            sock.send(f"[SERVER] Tus artefactos son {', '.join(art_list)}. ¿Esta bien? (Si/No)".encode())
+            try:
+                sino = sock.recv(1024).decode()
+            except:
+                break
+            if sino == "Si":
+                sock.send("[SERVER] ¡OK!".encode())
+                arte = art_list
+            else:
+                pass
+
     nombre = sock.getpeername()[1]
-    arte_dict[nombre] = ["12", "15"]
+    arte_dict[nombre] = arte
+
 
     while True:
         try:
